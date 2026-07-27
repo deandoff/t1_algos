f15ad2b7a8e143fc6cb7739bb809eb0a4d706756
deandoff <deandoff@gmail.com>
2026-07-27 13:48:05 +0300

    Homework1

---
+++ src/main/java/org/example/algos_1/Edge.java
@@ -0,0 +1,14 @@
+package org.example.algos_1;
+
+public class Edge {
+    int to;
+    int brandwidth;
+    int loss;
+
+    public Edge(int to, int brandwidth, int loss) {
+        this.to = to;
+        this.brandwidth = brandwidth;
+        this.loss = loss;
+    }
+}
+
